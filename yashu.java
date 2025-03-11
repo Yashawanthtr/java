c3645015fb5bfd1b7cf39464cfad0c8e23e79673
Yashawanthtr <yashwanthtr0@gmail.com>
2025-03-11 16:18:31 +0530

    Update yashu.java1

--- yashu.java
+++ yashu.java
@@ -1,7 +1,8 @@
 
 class yashu
 	{
-    public static void main(String[] args) {
+    public static void main(String[] args)
+		{
         System.out.println("hellow spt, welcome to jenkins);
     }
 }
